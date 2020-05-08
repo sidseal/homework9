2a3a876288be9ac2ee074dafc7da5d246a10832a
sidseal <sidseal@me.com>
2020-05-08 16:23:57 -0400

    LicenseLink

--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,5 +1,9 @@
 function generateMarkdown(data) {
+  const LicLink = `[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})`
+
   return `
+### ${LicLink}
+
 # ${data.title}
 `;
 }
