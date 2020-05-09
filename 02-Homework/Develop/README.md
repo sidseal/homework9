048a70e9a986c2489815b8b555a880e7f1e09d8b
sidseal <sidseal@me.com>
2020-05-09 00:38:11 -0400

    working on TOC

--- 02-Homework/Develop/README.md
+++ 02-Homework/Develop/README.md
@@ -1,11 +1,32 @@
 
-### [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-# test101
+ [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+ 
+# ladeeeeDA
 This README.md generator is a command line application that will take some inputs from the user and create a README.md file for the repo
-of the user's choice. This app can be initialized on your command line by using the command:
+of the user's choice. This app can be initialized on your command line by using the following command:
 
 ```node index.js```
+
+# Description:
+``````
+
+
+
+
+# Table of Contents:
+*[Licence](#License)
+*[Installation](#Installation)
+*[Initialization](#Initialization)
+*[Tests](#Tests)
+*[Usage](#Usage)
+*[Contribution](#Contribution)
+
+# License:
+This program is licensed under the MIT license.
+
+
+
+
 
 
 
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -2,13 +2,34 @@ function generateMarkdown(data) {
   const LicLink = `[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})`
 
   return `
-### ${LicLink}
-
+ ${LicLink}
+ 
 # ${data.title}
 This README.md generator is a command line application that will take some inputs from the user and create a README.md file for the repo
-of the user's choice. This app can be initialized on your command line by using the command:
+of the user's choice. This app can be initialized on your command line by using the following command:
 
 \`\`\`${data.initialize}\`\`\`
+
+# Description:
+\`\`\`${data.description}\`\`\`
+
+
+
+
+# Table of Contents:
+*[Licence](#License)
+*[Installation](#Installation)
+*[Initialization](#initialization)
+*[Tests](#tests)
+*[Usage](#usage)
+*[Contribution](#contribution)
+
+# License:
+This program is licensed under the ${data.license} license.
+
+
+
+
 
 
 
